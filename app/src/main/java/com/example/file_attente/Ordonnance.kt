a2d5b2aaa5ad281d8736881630425a94f6eced47
Dakusy <dakusy@gmail.com>
2020-07-07 11:56:24 +0200

    Commit Android Studio Project + MainActivity Start

---
+++ app/src/main/java/com/example/file_attente/Ordonnance.kt
@@ -0,0 +1,11 @@
+package com.example.file_attente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ordonnance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ordonnance)
+    }
+}
