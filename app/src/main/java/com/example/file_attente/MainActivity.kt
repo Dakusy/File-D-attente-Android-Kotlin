5c6aa15b3527cbb5f47b0c2d8b7d2aa3b3a1fb78
Dakusy <dakusy@gmail.com>
2020-07-08 09:50:22 +0200

    added MainActivity buttons redirect on Prescription + Started Language Activity EN,FR

--- app/src/main/java/com/example/file_attente/MainActivity.kt
+++ app/src/main/java/com/example/file_attente/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.file_attente
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
-    
+
+
+    fun Go(v: View?) {
+        //on creer une nouvelle intent on definit la class de depart ici this et la class d'arrivé ici SecondActivite
+        val intent = Intent(this, Ordonnance::class.java)
+        //on lance l'intent, cela a pour effet de stoper l'activité courante et lancer une autre activite ici SecondActivite
+        startActivity(intent)
+    }
+
+    fun Language(v: View?){
+        val intent = Intent(this, Ordonnance::class.java)
+
+    }
+
+
 }
--- app/src/main/java/com/example/file_attente/Ordonnance.kt
+++ app/src/main/java/com/example/file_attente/Ordonnance.kt
@@ -2,10 +2,17 @@ package com.example.file_attente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Ordonnance : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ordonnance)
+    }
+
+    public fun Back(v: View?) {
+        //pour retourner a l’activite principale il suffit seulement d’appler la methode finish() qui vas tuer cette activite
+
+        finish();
     }
 }
